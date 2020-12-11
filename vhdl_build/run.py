409bc605c91319adafca1301853578b92155b1d5
Yogotie <2850309+yogotie@users.noreply.github.com>
2020-12-11 11:24:08 -0500

    cleaned up the run.py to remove unused variables

--- vhdl_build/run.py
+++ vhdl_build/run.py
@@ -2,9 +2,6 @@
 from pathlib import Path
 from vunit import VUnit
 
-SRC_PATH = Path(__file__).parent / "vhdl_src"
-UUT_PATH = Path(__file__).parent / "vhdl_uut"
-
 VU = VUnit.from_argv()
 
 UUT = VU.add_library("uut")
